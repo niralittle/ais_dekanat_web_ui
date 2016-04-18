3e50f85da0e0f53a9cdc03444ea0c8899f670444
Viktor Vaidner <space10@mail.ru>
2016-04-18 23:30:51 +0300

    Add module for Jackson JSON processor which handles Hibernate

--- src/main/java/com/kma/ais_dekanat/controller/StudentController.java
+++ src/main/java/com/kma/ais_dekanat/controller/StudentController.java
@@ -1,26 +1,23 @@
-package com.kma.ais_dekanat.resource_controller;
+package com.kma.ais_dekanat.controller;
 
 import com.kma.ais_dekanat.model.Student;
 import com.kma.ais_dekanat.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
-/**
- * Created by nira on 05.04.16.
- */
-@RestController
+@Controller
 public class StudentController {
-
     @Autowired
     StudentService studentService;
 
-    @RequestMapping(value = "/getAllStudents", method = RequestMethod.GET)
+    @RequestMapping(value = "student/all", method = RequestMethod.GET)
+    @ResponseBody
     public List<Student> getAllStudents() {
         return studentService.getAllStudents();
     }
-
 }
