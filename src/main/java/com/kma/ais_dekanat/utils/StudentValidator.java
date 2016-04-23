9dd3dc0e17d21c75f4433f6cf0e8b4f071b8a2d6
Kateryna Atamanchuk <ua.katerynka@gmail.com>
2016-04-23 21:25:19 +0300

    fixed build, left a TODO for @7space7

---
+++ src/main/java/com/kma/ais_dekanat/utils/StudentValidator.java
@@ -0,0 +1,13 @@
+package com.kma.ais_dekanat.utils;
+
+import com.kma.ais_dekanat.model.Student;
+import org.springframework.validation.BindingResult;
+
+/**
+ * Created by nira on 23.04.16.
+ */
+public class StudentValidator {
+    //TODO @7space7 don't forget to commit your code and replace this stub
+    public void validate(Student student, BindingResult result) {
+    }
+}
